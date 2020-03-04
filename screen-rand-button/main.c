e9d9a6a17c6422b22278940bb235fa8301fbaf45
Bruno De Masi <brunodemasi1@gmail.com>
2020-03-04 01:41:43 -0500

    fix: timer and math fixed, onClose displays info about the session

--- screen-rand-button/main.c
+++ screen-rand-button/main.c
@@ -9,9 +9,13 @@
 #define RANDOM_MAX 500
 
 GLfloat win;
-int timer_counter = 0, currBuff = 0;
+int timerCounter = 0, 
+		currBuff = 0, 
+		buttonClickCounter = 0, 
+		missedClickCounter = 0;
 
-int redraw_rate = 1000;
+int redrawRate = 1000;
+double reducer = 0.90;
 
 GLfloat x, y;
 void drawSquare(int x, int y, double sidelength)
@@ -38,10 +42,14 @@ void keyboard(int key, int x, int y) {
 }
 
 void mouse(int button, int state, int x, int y) {
-	float pixel[4];
+	float pixel[3];
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, &pixel);
-		printf("%f %f %f %f\n", pixel[0], pixel[1], pixel[2], pixel[3]);
+		glReadPixels(x, 500 - 1 - y, 1, 1, GL_RGB, GL_FLOAT, &pixel);
+		// printf("%f %f %f \n", pixel[0], pixel[1], pixel[2]);
+		if ((int) pixel[0] == 1) {
+			redrawRate = redrawRate * reducer;
+			buttonClickCounter++;
+		} else missedClickCounter++;
 	}
 }
 
@@ -49,59 +57,45 @@ int randomInRange(int min, int max){
    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
 
-// void counter() {
-// 	int oldSecond = 0;
-// 	time_t begin;
-// 	double time_spent;
-
-// 	begin = clock();
-// 	for (int i = 0; 1; i++) {
-// 		time_spent = (double) (clock() - begin) / CLOCKS_PER_SEC;
-		
-// 		if ((int) time_spent > oldSecond) {
-// 			int randX = randomInRange(-RANDOM_MAX, RANDOM_MAX);
-// 			int randY = randomInRange(-RANDOM_MAX, RANDOM_MAX);
-// 			drawSquare(randX, randY, 30);
-// 			glutPostRedisplay();
-// 		}
-
-// 		if (time_spent >= 2.0)
-// 			exit(0);
-
-// 		oldSecond = (int) time_spent;
-
-// 		sleep(1);
-// 	}
-// }
-
 void drawOnScreen(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	
 	glColor3f(1.0f, 0.0f, 0.0f);
 	drawSquare(x, y, 30);
 
-	glFlush();
 	glutSwapBuffers();
 }
 
 void redraw(void) {
-	if (timer_counter > currBuff) {
-		currBuff = timer_counter;
+	if (timerCounter > currBuff) {
+		currBuff = timerCounter;
 
 		x = (GLfloat) randomInRange(-RANDOM_MAX, RANDOM_MAX);
 		y = (GLfloat) randomInRange(-RANDOM_MAX, RANDOM_MAX);
-		
+
 		glutPostRedisplay();	
 	}
-
 }
 
 void timer(int extra) {
-	timer_counter++;
+	timerCounter += redrawRate;
 
-	if (timer_counter >= 60) exit(0);
+	if (timerCounter / 1000 >= 60) {
+		printf("\nResultados:\nClicks certos: %d\nClicks errados: %d\n", 
+			buttonClickCounter,
+			missedClickCounter
+		);
 
-	glutTimerFunc(redraw_rate, timer, 0);
+		float rateInMs = (float) redrawRate / 1000;
+		printf("A taxa de redução/click é de %.2f%%,\no redesenho foi de 1 segundo para: %.3fms\n\n", 
+			1 - reducer, 
+			rateInMs
+		);
+
+		exit(0);
+	}
+
+	glutTimerFunc(redrawRate, timer, 0);
 }
 
 void start(void) {
@@ -123,8 +117,7 @@ int main(int argc, char** argv) {
 	glutMouseFunc(mouse);
 	glutKeyboardFunc(keyboard);
 	glutIdleFunc(redraw);
-	glutTimerFunc(redraw_rate, timer, 0);
+	glutTimerFunc(redrawRate, timer, 0);
 	start();
 	glutMainLoop();
-	// counter();
 }
