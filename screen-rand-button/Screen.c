9881bd48bc807f41376c13bb7d950d2a36d8c089
Bruno De Masi <brunodemasi1@gmail.com>
2020-03-04 00:55:00 -0500

    add: button changes location every second

--- screen-rand-button/Screen.c
+++ screen-rand-button/Screen.c
@@ -1,20 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 #include <GL/glut.h>
-#include <math.h>
-#define PI 3.14159265f
 
-float w = 500.0f, h = 500.0f;
+#define THREAD_NUM 4
+#define RESOLUTION 500.0f
+#define RANDOM_MAX 500
 
-GLfloat ballRadius = 0.05f;
+GLfloat win;
+int timer_counter = 0, currBuff = 0;
 
-void Start(void) {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+int redraw_rate = 1000;
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(w, w, h, h);
-	glMatrixMode(GL_MODELVIEW);
+GLfloat x, y;
+void drawSquare(int x, int y, double sidelength)
+{
+    double halfside = sidelength / 2;
+
+    glColor3d(1, 0, 0);
+    glBegin(GL_POLYGON);
+
+    glVertex2d(x + halfside, y + halfside);
+    glVertex2d(x + halfside, y - halfside);
+    glVertex2d(x - halfside, y - halfside);
+    glVertex2d(x - halfside, y + halfside);
+
+    glEnd();
+}
+
+void keyboard(int key, int x, int y) {
+	switch(key) {
+		case 27:
+			exit(0);
+			break;
+	}
 }
 
 void mouse(int button, int state, int x, int y) {
@@ -25,39 +45,86 @@ void mouse(int button, int state, int x, int y) {
 	}
 }
 
-void display(void) {
-	float x = 0.0f, y = 0.0f;;
+int randomInRange(int min, int max){
+   return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
 
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+// void counter() {
+// 	int oldSecond = 0;
+// 	time_t begin;
+// 	double time_spent;
+
+// 	begin = clock();
+// 	for (int i = 0; 1; i++) {
+// 		time_spent = (double) (clock() - begin) / CLOCKS_PER_SEC;
+		
+// 		if ((int) time_spent > oldSecond) {
+// 			int randX = randomInRange(-RANDOM_MAX, RANDOM_MAX);
+// 			int randY = randomInRange(-RANDOM_MAX, RANDOM_MAX);
+// 			drawSquare(randX, randY, 30);
+// 			glutPostRedisplay();
+// 		}
+
+// 		if (time_spent >= 2.0)
+// 			exit(0);
+
+// 		oldSecond = (int) time_spent;
+
+// 		sleep(1);
+// 	}
+// }
+
+void drawOnScreen(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-
+	
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glBegin(GL_TRIANGLE_FAN);
-		glVertex2f(x, y);
-		int numSegments = 100;
-		GLfloat angle;
-		for (int i = 0; i <= numSegments; i++) {
-			angle = i * 2.0f * PI / numSegments;
-			//printf("%f\n", (GLfloat) angle);
-			
-			float x2 = x + sin(angle) * ballRadius;
-		      	float y2 = y + cos(angle) * ballRadius;	
-			
-			glVertex2f(x2, y2);
-		}
-	glEnd();
+	drawSquare(x, y, 30);
 
+	glFlush();
 	glutSwapBuffers();
+}
+
+void redraw(void) {
+	if (timer_counter > currBuff) {
+		currBuff = timer_counter;
+
+		x = (GLfloat) randomInRange(-RANDOM_MAX, RANDOM_MAX);
+		y = (GLfloat) randomInRange(-RANDOM_MAX, RANDOM_MAX);
+		
+		glutPostRedisplay();	
+	}
+
+}
+
+void timer(int extra) {
+	timer_counter++;
+
+	if (timer_counter >= 60) exit(0);
+
+	glutTimerFunc(redraw_rate, timer, 0);
+}
+
+void start(void) {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	win = RESOLUTION;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-win, win, -win, win);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(w, h);
+	glutInitWindowSize(RESOLUTION, RESOLUTION);
 	glutInitWindowPosition(300, 300);
-	glutCreateWindow("Click on the button");
-	glutDisplayFunc(display);
-	glutMouseFunc(mouse);	
-	Start();
+	glutCreateWindow("Desenhador de Linha");
+	glutDisplayFunc(drawOnScreen);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
+	glutIdleFunc(redraw);
+	glutTimerFunc(redraw_rate, timer, 0);
+	start();
 	glutMainLoop();
+	// counter();
 }
